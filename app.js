da4aa4a04f59a79f1263bfe940209bc5e57bee81
ayveren <ayverend@gmail.com>
2018-04-25 18:43:13 +0300

    add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
+const mongoose = require('mongoose');
 
-var port = process.env.PORT || 3000;
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+
+const book = require('/model/bookModel');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const bookRouter = express.Router();
+
+bookRouter.route('/books')
+    .get(function (req,res) {
+        const responseJSON = {hello: 'This is my api'};
+        res.json(responseJSON)
+    });
+
+app.use('/api', bookRouter);
+
+
 
 app.get('/', function(req, res){
     res.send('Welcome to API');
---
+++ models/bookModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+const bookModel = new Schema({
+    title: {
+        type: String
+    },
+    genre: {
+        type: String
+    },
+    author: {
+        type: String
+    },
+    read: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('Book', bookModel);
