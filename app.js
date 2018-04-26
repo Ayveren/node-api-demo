e3e9819f6a1d2d610f08239202c8e184f87f31dd
ayveren <ayverend@gmail.com>
2018-04-26 12:00:03 +0300

    add mongoose schema

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 
-const book = require('/model/bookModel');
+const Book = require('./models/bookModel');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -12,19 +12,24 @@ const port = process.env.PORT || 3000;
 const bookRouter = express.Router();
 
 bookRouter.route('/books')
-    .get(function (req,res) {
-        const responseJSON = {hello: 'This is my api'};
-        res.json(responseJSON)
+    .get(function (req, res) {
+        Book.find(function (err, books) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.json(books)
+            }
+        });
     });
 
 app.use('/api', bookRouter);
 
 
-
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send('Welcome to API');
 });
 
-app.listen(port, function(){
+app.listen(port, function () {
     console.log('Running on PORT: ' + port);
 });
