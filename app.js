8d84c493dced059b120291881de228e195b1c5ca
ayveren <ayverend@gmail.com>
2018-04-30 14:08:39 +0300

    add filtering

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ const bookRouter = express.Router();
 
 bookRouter.route('/books')
     .get(function (req, res) {
-        Book.find(function (err, books) {
+
+        let query = {};
+        if(req.query.genre){
+            query.genre = req.query.genre;
+        }
+        Book.find(query, function (err, books) {
             if (err) {
                 res.status(500).send(err);
             }
@@ -23,8 +28,19 @@ bookRouter.route('/books')
         });
     });
 
-app.use('/api', bookRouter);
+bookRouter.route('/books/:bookId')
+    .get(function (req, res) {
+        Book.findById(req.params.bookId, function (err, book) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            else {
+                res.json(book)
+            }
+        });
+    });
 
+app.use('/api', bookRouter);
 
 app.get('/', function (req, res) {
     res.send('Welcome to API');
